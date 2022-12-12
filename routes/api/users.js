155a8a999bf6a5d29b50607f7d988d08270b2511
santhosh-jaye <santhoshjagadeesanbiz@gmail.com>
2022-12-12 20:08:21 +0530

    minor updates to request and body validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -19,7 +19,7 @@ router.post(
   (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      res.status(400).json({ message: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
     res.send('User route');
   }
